2b3762e7607e931f5739d4955c30f935f8fc9bb9
John Doe <2896884101@qq.com>
2020-04-23 23:29:30 +0800

    基于网络通信的服务端和客户端

---
+++ 01 理解网络编程和套接字/client.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <string>
+#include <sys/types.h>       
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <string.h>
+#include <unistd.h>
+
+/* 步骤分析
+ * 1: socket(PF_INET,SOCK_STREAM,0)
+ * 2: connect(serv_sock,(sockaddr*)&serv_addr,sizeof(serv_addr)
+ * 3: read、write函数进行数据通信
+*/
+
+int main(int argc,char** argv) {
+    int serv_sock;
+    char message[30];
+    int str_len = 0;
+
+    serv_sock = socket(PF_INET,SOCK_STREAM,0);
+    if (serv_sock == -1){
+        std::cout << "create socket failed" << std::endl;
+    }
+    
+    sockaddr_in serv_addr;
+    memset(&serv_addr,0,sizeof(serv_addr));
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_addr.s_addr = inet_addr(argv[1]);
+    serv_addr.sin_port = htons(atoi(argv[2]));
+    
+    if (connect(serv_sock,(sockaddr*)&serv_addr,sizeof(serv_addr)) == -1){
+        std::cout << "connet socket failed" << std::endl;
+    }
+    
+    str_len = read(serv_sock,message,sizeof(message));
+    if (str_len == -1){
+        std::cout << "read socket failed." << std::endl;
+    }
+    std::cout <<"recv message: " << message << std::endl;
+
+    close(serv_sock);
+    
+    return 0;
+}
